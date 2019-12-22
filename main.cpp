06c29cdde33723141eee81d6dc6afa0a80ede76b
chencongyong <ndsf@qq.com>
2019-12-23 00:01:03 +0800

    Finish generating code

--- main.cpp
+++ main.cpp
@@ -26,7 +26,127 @@ void Tokenize(std::istream &input, std::ostream &output) {
     return;
 }
 
-void Analyse(std::istream &input, std::ostream &output) {
+std::string getLow(int n, int in) {
+    std::string bt;
+    for (int i = 1; i <= n; i++) {
+        int shiftNum = 8 * (n - i);
+        bt.push_back((char) ((in >> shiftNum) & 0xff));
+    }
+    return bt;
+}
+
+char getByte(c0::Operation op) {
+    switch (op) {
+        case c0::IPUSH:
+            return 0x02;
+        case c0::LOADA:
+            return 0x0a;
+        case c0::ILOAD:
+            return 0x10;
+        case c0::IMUL:
+            return 0x38;
+        case c0::IDIV:
+            return 0x3c;
+        case c0::IADD:
+            return 0x30;
+        case c0::ISUB:
+            return 0x34;
+        case c0::RET:
+            return 0x88;
+        case c0::IRET:
+            return 0x89;
+        case c0::JE:
+            return 0x71;
+        case c0::JNE:
+            return 0x72;
+        case c0::JL:
+            return 0x73;
+        case c0::JGE:
+            return 0x74;
+        case c0::JG:
+            return 0x75;
+        case c0::JLE:
+            return 0x76;
+        case c0::ICMP:
+            return 0x44;
+        case c0::JMP:
+            return 0x70;
+        case c0::CALL:
+            return 0x80;
+        case c0::ISTORE:
+            return 0x20;
+        case c0::SPRINT:
+            return 0xa3;
+        case c0::IPRINT:
+            return 0xa0;
+        case c0::CPRINT:
+            return 0xa2;
+        case c0::LOADC:
+            return 0x09;
+        case c0::ISCAN:
+            return 0xb0;
+        case c0::CSCAN:
+            return 0xb2;
+        case c0::NOP:
+            return 0x00;
+        case c0::POP:
+            return 0x04;
+        case c0::PRINTL:
+            return 0xaf;
+        case c0::BIPUSH:
+            return 0x01;
+    }
+}
+
+bool isJump(c0::Operation type) { // need removing
+    return (type == c0::JMP || type == c0::JNE || type == c0::JE
+            || type == c0::JG || type == c0::JL || type == c0::JLE || type == c0::JGE
+    );
+}
+
+int32_t needParameterNum(c0::Operation type) {
+    if (type == c0::LOADA)
+        return 2;
+    else if (type == c0::IPUSH || type == c0::LOADC || isJump(type) ||
+             type == c0::CALL)
+        return 1;
+    else
+        return 0;
+}
+
+void getObjByType(c0::Operation type, std::ostream &output, c0::Instruction instruction) {
+    std::string op;
+    switch (needParameterNum(type)) {
+        case 1:
+            if (instruction.GetOperation() == c0::IPUSH) {
+                op = getLow(4, instruction.GetX());
+                output << op;
+            } else if (instruction.GetOperation() == c0::LOADC) {
+                op = getLow(2, instruction.GetX());
+                output << op;
+            } else if (instruction.GetOperation() == c0::CALL) {
+                op = getLow(2, instruction.GetX());
+                output << op;
+            } else if (isJump(instruction.GetOperation())) {
+                op = getLow(2, instruction.GetX());
+                output << op;
+            } else if (instruction.GetOperation() == c0::BIPUSH) {
+                op = getLow(1, instruction.GetX());
+                output << op;
+            }
+            break;
+        case 2:
+            op = getLow(2, instruction.GetX());
+            output << op;
+            op = getLow(4, instruction.GetY());
+            output << op;
+            break;
+        case 0:
+            break;
+    }
+}
+
+void Analyse(std::istream &input, std::ostream &output, bool generateText) {
     auto tks = _tokenize(input);
     c0::Analyser analyser(tks);
 
@@ -37,60 +157,136 @@ void Analyse(std::istream &input, std::ostream &output) {
         fmt::print(stderr, "Syntactic analysis error: {}\n", err.value());
         exit(2);
     }
-    output << ".constants:\n";
-
-    for (auto constant : constants) {
-//        switch (constant.GetType()) {
-//            case c0::STRING_TYPE:
-        auto result = std::any_cast<std::string>(constant.GetValue());
-        result = regex_replace(result, std::regex("\t"), "\\x09");
-        result = regex_replace(result, std::regex("\n"), "\\x0A");
-        result = regex_replace(result, std::regex("\r"), "\\x0D");
-        output << fmt::format("{} S \"{}\"\n", constant.GetIndex(), result);
-//                break;
-//            case c0::INTEGER_TYPE:
-//                output << fmt::format("{} I {}\n", constant.GetIndex(),
-//                                      std::any_cast<std::int32_t>(constant.GetValue()));
-//                break;
-//            case c0::FLOAT_TYPE: // WIP
-//            default:
-//                output << fmt::format("{} D {}\n", constant.GetIndex(),
-//                                      std::any_cast<std::string>(constant.GetValue()));
-//                break; // cannot handle with other types
-//        }
-    }
-
-    output << ".start:\n";
-    bool sm = false;
-    int funcIndex = 0;
-    int j = 0;
-    for (int i = 0; i < instructions.size(); i++) {
-        auto instruction = instructions[i];
-        if (instructions[i].GetIndex() == 0) {
-            if (sm) {
-                j = i;
-                break;
+    if (generateText) {
+        output << ".constants:\n";
+        for (auto constant : constants) {
+            std::string result;
+            switch (constant.GetType()) {
+                case c0::STRING_TYPE:
+                    result = std::any_cast<std::string>(constant.GetValue());
+                    result = regex_replace(result, std::regex("\t"), "\\x09");
+                    result = regex_replace(result, std::regex("\n"), "\\x0A");
+                    result = regex_replace(result, std::regex("\r"), "\\x0D");
+                    output << fmt::format("{} S \"{}\"\n", constant.GetIndex(), result);
+                    break;
+                case c0::INTEGER_TYPE:
+                    output << fmt::format("{} I {}\n", constant.GetIndex(),
+                                          std::any_cast<std::int32_t>(constant.GetValue()));
+                    break;
+                case c0::FLOAT_TYPE: // WIP
+                default:
+                    output << fmt::format("{} D {}\n", constant.GetIndex(),
+                                          std::any_cast<std::string>(constant.GetValue()));
+                    break; // cannot handle with other types
             }
-            sm = true;
         }
-        output << fmt::format("{}\n", instruction);
-    }
 
-    output << ".functions:\n";
-    for (auto function : functions)
-        output << fmt::format("{} {} {} {}\n", function.GetIndex(), function.GetNameIndex(), function.GetParamsSize(),
-                              function.GetLevel());
-    sm = false;
-    funcIndex = 0;
-    for (int i = j; i < instructions.size(); i++) {
-        auto instruction = instructions[i];
-        if (instructions[i].GetIndex() == 0) {
-            output << fmt::format(".F{}:\n", funcIndex);
-            funcIndex++;
+        output << ".start:\n";
+        bool sm = false;
+        int funcIndex = 0;
+        int j = 0;
+        for (int i = 0; i < instructions.size(); i++) {
+            auto instruction = instructions[i];
+            if (instructions[i].GetIndex() == 0) {
+                if (sm) {
+                    j = i;
+                    break;
+                }
+                sm = true;
+            }
+            output << fmt::format("{}\n", instruction);
         }
-        output << fmt::format("{}\n", instruction);
-        if (funcIndex > functions.size())
-            break;
+
+        output << ".functions:\n";
+        for (auto function : functions)
+            output << fmt::format("{} {} {} {}\n", function.GetIndex(), function.GetNameIndex(),
+                                  function.GetParamsSize(),
+                                  function.GetLevel());
+        sm = false;
+        funcIndex = 0;
+        for (int i = j; i < instructions.size(); i++) {
+            auto instruction = instructions[i];
+            if (instructions[i].GetIndex() == 0) {
+                output << fmt::format(".F{}:\n", funcIndex);
+                funcIndex++;
+            }
+            output << fmt::format("{}\n", instruction);
+            if (funcIndex > functions.size())
+                break;
+        }
+    } else {
+        output.put(0x43);
+        output.put(0x30);
+        output.put(0x3a);
+        output.put(0x29);
+        output.put(0x00);
+        output.put(0x00);
+        output.put(0x00);
+        output.put(0x01);
+        output.put((char) ((constants.size() >> 8) & 0xff));
+        output.put((char) (constants.size() & 0xff));
+        for (auto constant : constants) {
+            std::string result;
+            switch (constant.GetType()) {
+                case c0::STRING_TYPE:
+                    result = std::any_cast<std::string>(constant.GetValue());
+                    output.put(0x00); // type=STRING
+                    output.put((char) ((result.length() >> 8) & 0xff));
+                    output.put((char) (result.length() & 0xff));
+                    output << result;
+                    break;
+                default:
+                    break; // WIP
+            }
+        }
+        bool sm = false;
+        int funcIndex = 0;
+        int j = 0;
+
+        std::vector<int> arr;
+        int count = 0;
+        for (auto instruction: instructions) {
+            if (instruction.GetIndex() == 0) {
+                arr.push_back(count);
+                count = 0;
+            }
+            count++;
+        }
+        arr.push_back(count);
+
+
+        output << getLow(2, arr[1]);
+
+        for (int i = 0; i < instructions.size(); i++) {
+            auto instruction = instructions[i];
+            if (instructions[i].GetIndex() == 0) {
+                if (sm) {
+                    j = i;
+                    break;
+                }
+                sm = true;
+            }
+            // output << fmt::format("{}\n", instruction);
+            output.put(getByte(instruction.GetOperation()));
+            getObjByType(instruction.GetOperation(), output, instruction);
+        }
+
+        output << getLow(2, functions.size());
+        int cnt = 0;
+        for (int i = 2; i < arr.size(); i++) {
+            auto fun = functions[i - 2];
+
+            output << getLow(2, fun.GetNameIndex());
+            output << getLow(2, fun.GetParamsSize());
+            output << getLow(2, fun.GetLevel());
+            output << getLow(2, arr[i]);
+            for (int k = 0; k < arr[i]; k++) {
+                auto instruction = instructions[cnt + k + arr[1]];
+                output << getByte(instruction.GetOperation());
+                getObjByType(instruction.GetOperation(), output, instruction);
+            }
+            cnt += arr[i];
+        }
     }
     return;
 }
@@ -107,6 +303,10 @@ int main(int argc, char **argv) {
             .default_value(false)
             .implicit_value(true)
             .help("perform syntactic analysis for the input file.");
+    program.add_argument("-c")
+            .default_value(false)
+            .implicit_value(true)
+            .help("generate some random awesome code.");
     program.add_argument("-o", "--output")
             .required()
             .default_value(std::string("-"))
@@ -152,7 +352,9 @@ int main(int argc, char **argv) {
     if (program["-t"] == true) {
         Tokenize(*input, *output);
     } else if (program["-s"] == true) {
-        Analyse(*input, *output);
+        Analyse(*input, *output, true);
+    } else if (program["-c"] == true) {
+        Analyse(*input, *output, false);
     } else {
         fmt::print(stderr, "You must choose tokenization or syntactic analysis.");
         exit(2);
